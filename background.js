c6905b3e8a0ffe1c1b14ddeae693b916c58f3f40
sovia8 <109529298+sovia8@users.noreply.github.com>
2023-09-29 12:59:20 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,60 +1,83 @@
-// Function to request the current URL from the active tab
-function getCurrentUrl(callback) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const url = tabs[0].url;
-        callback(url);
-      } else {
-        callback(null);
-      }
+// background.js
+
+// Function to store the access token securely in chrome.storage
+function storeAccessToken(accessToken) {
+    chrome.storage.local.set({ 'access_token': accessToken }, function() {
+        console.log('Access token stored successfully.');
     });
-  }
-  
-  // Function to send a message to the content script with the current URL
-  function sendCurrentUrlMessage() {
-    getCurrentUrl((url) => {
-      if (url) {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.sendMessage(tab.id, { action: 'setUrl', url: url }, (response) => {
-            if (response && response.tempo) {
-              updatePopup(response.tempo);
-            } else {
-              updatePopup(null);
-            }
-          });
+}
+
+// Function to retrieve the access token from chrome.storage
+function getAccessToken(callback) {
+    chrome.storage.local.get(['access_token'], function(result) {
+        const accessToken = result.access_token;
+        if (accessToken) {
+            callback(accessToken);
+        } else {
+            console.error('Access token not found.');
+        }
+    });
+}
+
+// Function to fetch song information using the access token
+function getSongInfo(accessToken) {
+    const apiUrl = 'https://api.spotify.com/v1/me/player/currently-playing';
+
+    fetch(apiUrl, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Extract BPM and perform any desired actions with the song data.
+        const bpm = data.track.tempo;
+        console.log(`BPM: ${bpm}`);
+        // You can send this BPM value to your popup.js or perform other actions here.
+    })
+    .catch(error => {
+        console.error('Error fetching song information:', error);
+    });
+}
+
+// Listener for messages from popup.js
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === 'exchangeCodeForToken') {
+        const code = request.code;
+        const clientId = 'f9e8675000f54ddf9e0c21c143b6e4b3'; // Replace with your actual Spotify client ID
+        const clientSecret = '67b4d9634f584e5c8c7197a08a5b0892'; // Replace with your actual Spotify client secret
+        const redirectUri = 'https://ahaccdcojcnmdijhmbgehnbaljjakeac.chromiumapp.org/oauth2'; // Replace with your extension's ID
+        const tokenUrl = 'https://accounts.spotify.com/api/token';
+
+        // Prepare the request body
+        const data = new URLSearchParams();
+        data.append('code', code);
+        data.append('grant_type', 'authorization_code');
+        data.append('redirect_uri', redirectUri);
+
+        // Create the request headers for basic authentication
+        const headers = {
+            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        // Make a POST request to exchange code for tokens
+        fetch(tokenUrl, {
+            method: 'POST',
+            body: data,
+            headers: headers,
+        })
+        .then(response => response.json())
+        .then(tokens => {
+            // Store the access token securely
+            const accessToken = tokens.access_token;
+            storeAccessToken(accessToken);
+
+            // Send the tokens back to popup.js for storage
+            chrome.runtime.sendMessage({ action: 'storeTokens', tokens });
+        })
+        .catch(error => {
+            console.error('Error exchanging code for tokens:', error);
         });
-      }
-    });
-  }
-  
-  // Function to update the popup with the tempo value
-  function updatePopup(tempo) {
-    chrome.action.setPopup({ popup: 'popup.html' }, () => {
-      chrome.runtime.sendMessage({ action: 'updateTempo', tempo: tempo });
-    });
-  }
-  
-  // Event listener for receiving messages from content script or popup
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'getCurrentUrl') {
-      getCurrentUrl((url) => {
-        sendResponse({ url: url });
-      });
-      return true;
     }
-  });
-  
-  // Event listener for extension startup
-  chrome.runtime.onStartup.addListener(sendCurrentUrlMessage);
-  
-  // Event listener for extension installation or update
-  chrome.runtime.onInstalled.addListener(sendCurrentUrlMessage);
-  
-  // Event listener for tab updates
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.active) {
-      sendCurrentUrlMessage();
-    }
-  });
-  
+});
