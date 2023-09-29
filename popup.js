c5c676936a94488bd37be2cddf8755438471e853
sovia8 <109529298+sovia8@users.noreply.github.com>
2023-09-29 12:39:12 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,36 @@
-function getBPMFromContentScript() {
-  return new Promise((resolve) => {
-    chrome.runtime.sendMessage({ action: 'getBPM' }, (response) => {
-      resolve(response);
+// Function to initiate the Spotify authentication flow
+function authenticateWithSpotify() {
+    // Redirect the user to Spotify's authorization URL.
+    const clientId = 'YOUR_CLIENT_ID'; // Replace with your actual Spotify client ID
+    const redirectUri = chrome.identity.getRedirectURL();
+
+    // Define the scopes you need (e.g., user-read-playback-state) and encode them.
+    const scopes = encodeURIComponent('user-read-playback-state');
+    
+    // Construct the authentication URL
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&response_type=code`;
+
+    // Launch the authentication URL in a new tab
+    chrome.tabs.create({ url: authUrl }, (tab) => {
+        // Listen for changes in the URL of the newly created tab.
+        // When the user is redirected back to your extension, we'll capture the code.
+        chrome.webNavigation.onCompleted.addListener(function onTabComplete(details) {
+            if (details.tabId === tab.id) {
+                // Extract the code from the URL
+                const code = new URL(details.url).searchParams.get('code');
+                if (code) {
+                    // Send the code to your background script for token exchange.
+                    chrome.runtime.sendMessage({ action: 'getToken', code });
+                }
+                // Close the tab since we don't need it anymore
+                chrome.tabs.remove(tab.id);
+                // Remove the listener to prevent capturing other tab completions
+                chrome.webNavigation.onCompleted.removeListener(onTabComplete);
+            }
+        });
     });
-  });
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const response = await getBPMFromContentScript();
-  if (response && response.bpm) {
-    document.getElementById('bpm-value').textContent = 'BPM: ' + response.bpm;
-  } else {
-    document.getElementById('bpm-value').textContent = 'BPM not available.';
-  }
-});
+// Add an event listener to trigger authentication when a button is clicked.
+document.getElementById('auth-button').addEventListener('click', authenticateWithSpotify);
+
